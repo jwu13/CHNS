21afaf50683f50e61f656af4a8d4283e4cc5ada5
Junru Wu <jwu13@smith.edu>
2022-12-08 22:10:35 -0500

    adding two cnhs datasets

---
+++ R/process_datasets.R
@@ -0,0 +1,25 @@
+#' Annual Income from CHNS survey from 1989 to 2011
+#'
+#' @format ## `indinc_10_clean`
+#' A data frame with 62,029 rows and 32 columns:
+#' \describe{
+#'   \item{IDind}{individual identification number}
+#'   \item{wave}{survey year}
+#'   \item{indwage}{annual wage, bonus, and other income}
+#' }
+#' @source <https://www.cpc.unc.edu/projects/china>
+"indinc_10_clean"
+#'
+#'
+#' Number of ever married women and number of children
+#' @format ## `emw_12_clean`
+#' A data frame with 26,616 rows and 60 columns:
+#' \describe{
+#'   \item{IDind}{individual identification number}
+#'   \item{wave}{survey year}
+#'   \item{child_died}{Number of Children who died}
+#'   \item{child_births}{Total number of Children given births to}
+#'   ...
+#' }
+#' @source <https://www.cpc.unc.edu/projects/china>
+"emw_12_clean"
---
+++ data-raw/DATASET.R
@@ -0,0 +1,37 @@
+## code to prepare `emw_12` and `indinc_10` dataset goes here
+
+## clean indinc_10
+
+library(tidyverse)
+library(haven)
+library(janitor)
+
+indinc_10 <-read_sas("/Users/junruwu/Desktop/Fall 2022/SDS 270/chns/data-raw/indinc_10.sas7bdat")
+indinc_10_clean <- indinc_10 %>%
+  select(IDind, wave, indwage)%>%
+  mutate(indwage = ifelse(indwage < 0 , NA, indwage))%>%
+  rename(
+    individual_wage = indwage
+  )
+
+usethis::use_data(indinc_10_clean, overwrite = TRUE)
+
+
+# Clean emw_12 dataset
+library(tidyverse)
+library(haven)
+library(janitor)
+
+# ever_married_women
+emw_12 <-read_sas("/Users/junruwu/Desktop/Fall 2022/SDS 270/chns/data-raw/emw_12.sas7bdat")
+emw_12_clean <- emw_12 %>%
+  select(IDind, wave, S47, S47A) %>%
+  mutate(S47 = ifelse(S47 < 0 , NA, S47)) %>%
+  mutate(S47A = ifelse(S47A < 0 , NA, S47A)) %>%
+  rename(
+    child_died = S47,
+    child_births = S47A
+  )
+usethis::use_data(emw_12_clean, overwrite = TRUE)
+
+
