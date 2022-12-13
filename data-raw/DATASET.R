58c81dc6ef4e1373b589733ca63f360c65a72544
Junru Wu <jwu13@smith.edu>
2022-12-13 10:54:12 -0500

    construct map_dat dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -34,31 +34,25 @@ usethis::use_data(emw_12_clean, overwrite = TRUE)
 
 
 # map_dat
-indinc_10 <-read_sas("/Users/dd/Desktop/270/CHNS/data-raw/indinc_10.sas7bdat")
+indinc_10 <-read_sas("/Users/junruwu/Desktop/Fall 2022/SDS 270/chns/data-raw/indinc_10.sas7bdat")
 map_dat <- indinc_10 %>%
   select(wave, indwage, t1)%>%
   mutate(indwage = ifelse(indwage < 0 , NA, indwage))%>%
   na.omit(indwage)%>%
   group_by(t1, wave)%>%
-  summarize(wage_mean = mean(indwage))
-
-map_dat <- map_dat %>%
-  rename(
-    province = t1
-  )
-
-map_dat <- map_dat %>%
-  mutate(wage_mean = case_when(
-    province == 11   ~ beijing,
-    province == 21 ~ liaoning,
-    province == 23 ~ heilongjiang,
-    province == 31 ~ shanghai,
-    province == 32 ~ jiangsu,
-    province == 37 ~ shandong,
-    province == 41 ~ henan,
-    province == 42   ~ hubei,
-    province == 43 ~ hunan,
-    province == 45 ~ guangxi,
-    province ==52 ~ guizhou,
-    province == 55 ~ chongqing))
+  summarize(wage_mean = mean(indwage)) %>%
+  rename(province = t1) %>%
+  mutate(province = case_when(
+    province == 11 ~ "beijing",
+    province == 21 ~ "liaoning",
+    province == 23 ~ "heilongjiang",
+    province == 31 ~ "shanghai",
+    province == 32 ~ "jiangsu",
+    province == 37 ~ "shandong",
+    province == 41 ~ "henan",
+    province == 42 ~ "hubei",
+    province == 43 ~ "hunan",
+    province == 45 ~ "guangxi",
+    province == 52 ~ "guizhou",
+    province == 55 ~ "chongqing"))
 
