8d8f4bebb8fcb447f57e338b5d6a59e5c8bd0ddd
Betty Pu <bpu@smith.edu>
2022-12-18 18:47:10 -0500

    updated map_dat name to be consistent

--- R/map_income.R
+++ R/map_income.R
@@ -3,7 +3,7 @@ globalVariables(c("map_dat","indwage","wave","province", "year"))
 #'
 #' Provides the map of annual province wage an inputted base year (1989,1991,1993,1997,2000,2004,2006,2009,2011,2015) for provinces included in the survey.
 #'
-#' @param year survey year. Find possible years in the  `map_dat` data frame.
+#' @param year survey year. Find possible years in the  `map_dat_clean` data frame.
 #' @return A map with the annual average income mapped on China's map for provinces in the survey. For provinces that are not included in the survey, the color is grey.
 #' @examples
 #' map_income(2015)
@@ -19,19 +19,19 @@ globalVariables(c("map_dat","indwage","wave","province", "year"))
 
     # Error Handling
 
-    if (!(year %in% merged_map_dat$wave))
+    if (!(year %in% map_dat_clean$wave))
     { stop("Please input a survey year listed in the `map_data` data frame as base year.")}
 
-    if (year == 1989) {df <- filter (merged_map_dat, wave == 1989)}
-    if (year == 1991) {df <- filter (merged_map_dat, wave == 1991)}
-    if (year == 1993) {df <- filter (merged_map_dat, wave == 1993)}
-    if (year == 1997) {df <- filter (merged_map_dat, wave == 1997)}
-    if (year == 2000) {df <- filter (merged_map_dat, wave == 2000)}
-    if (year == 2004) {df <- filter (merged_map_dat, wave == 2004)}
-    if (year == 2006) {df <- filter (merged_map_dat, wave == 2006)}
-    if (year == 2009) {df <- filter (merged_map_dat, wave == 2009)}
-    if (year == 2011) {df <- filter (merged_map_dat, wave == 2011)}
-    if (year == 2015) {df <- filter (merged_map_dat, wave == 2015)}
+    if (year == 1989) {df <- filter (map_dat_clean, wave == 1989)}
+    if (year == 1991) {df <- filter (map_dat_clean, wave == 1991)}
+    if (year == 1993) {df <- filter (map_dat_clean, wave == 1993)}
+    if (year == 1997) {df <- filter (map_dat_clean, wave == 1997)}
+    if (year == 2000) {df <- filter (map_dat_clean, wave == 2000)}
+    if (year == 2004) {df <- filter (map_dat_clean, wave == 2004)}
+    if (year == 2006) {df <- filter (map_dat_clean, wave == 2006)}
+    if (year == 2009) {df <- filter (map_dat_clean, wave == 2009)}
+    if (year == 2011) {df <- filter (map_dat_clean, wave == 2011)}
+    if (year == 2015) {df <- filter (map_dat_clean, wave == 2015)}
 
     # divide average income into categories for mapping
     # df$wage_mean_new = cut(df$wage_mean, c(0, 5000, 10000, 15000, 20000, 30000, 40000))
