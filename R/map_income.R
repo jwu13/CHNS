fb0b07c1a3c38cea140223e159c02dc2a3de3b3a
Betty Pu <bpu@smith.edu>
2022-12-18 20:16:21 -0500

    change map_income function to make it reproductive

--- R/map_income.R
+++ R/map_income.R
@@ -22,29 +22,15 @@ globalVariables(c("map_dat","indwage","wave","province", "year"))
     if (!(year %in% map_dat_clean$wave))
     { stop("Please input a survey year listed in the `map_data` data frame as base year.")}
 
-    if (year == 1989) {df <- filter (map_dat_clean, wave == 1989)}
-    if (year == 1991) {df <- filter (map_dat_clean, wave == 1991)}
-    if (year == 1993) {df <- filter (map_dat_clean, wave == 1993)}
-    if (year == 1997) {df <- filter (map_dat_clean, wave == 1997)}
-    if (year == 2000) {df <- filter (map_dat_clean, wave == 2000)}
-    if (year == 2004) {df <- filter (map_dat_clean, wave == 2004)}
-    if (year == 2006) {df <- filter (map_dat_clean, wave == 2006)}
-    if (year == 2009) {df <- filter (map_dat_clean, wave == 2009)}
-    if (year == 2011) {df <- filter (map_dat_clean, wave == 2011)}
-    if (year == 2015) {df <- filter (map_dat_clean, wave == 2015)}
+    # filter by the input year
+    df <- filter(map_dat_clean, wave == year)
 
-    # divide average income into categories for mapping
-    # df$wage_mean_new = cut(df$wage_mean, c(0, 5000, 10000, 15000, 20000, 30000, 40000))
-    # df_new <- data.frame(df)
-
-    # This following code works
-    #merged_1993 <- filter(merged_map_dat, wave ==1993)
-    #geojsonMap(merged_1993, "china", namevar = ~name, valuevar = ~wage_mean)
-
-    map <- geojsonMap(df, "china", namevar = ~name, valuevar = ~wage_mean, palette = topo.colors(3), colorMethod = "factor")
+    # map the data
+    map <- geojsonMap(df, "china", namevar = ~name, valuevar = ~wage_mean_cat, colorMethod = "factor")
 
     return(map)
   }
+
 
 
 
