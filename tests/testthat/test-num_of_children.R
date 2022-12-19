b461e3f36e66cc217657a053716facf2313e16c2
Betty Pu <bpu@smith.edu>
2022-12-19 09:39:00 -0500

    load packages in vignette

---
+++ tests/testthat/test-num_of_children.R
@@ -0,0 +1,9 @@
+library(testthat)
+library(dplyr)
+
+num_of_children_test <- num_of_children(year = 2006)
+
+testthat::test_that("The number of distinct women in the 2006 dataset equals our expectation", {
+  expect_equal(ncol(num_of_children_test), 5)
+  expect_equal(nrow(num_of_children_test), 385)
+})
