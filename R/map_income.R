1a45e9a5784c4cea2a07235d40d0c0d452bb8d8d
Betty Pu <bpu@smith.edu>
2022-12-20 19:24:36 -0500

    change import to importFrom

--- R/map_income.R
+++ R/map_income.R
@@ -1,16 +1,15 @@
 globalVariables(c("map_dat_clean","indwage","wave","province", "year", "wage_mean_cat"))
-#' Function for mapping average income by year and provinces included in the survey
+#' Function for mapping average income by year and provinces included in the survey for provinces included in the survey.
 #'
-#' Provides the map of annual province wage an inputted base year (1989,1991,1993,1997,2000,2004,2006,2009,2011,2015) for provinces included in the survey.
-#'
-#' @param year survey year. Find possible years in the  `map_dat_clean` data frame.
+#' Provides the map of annual province wage an inputted base year
+#' @param year valid survey year. (1989, 1991, 1993, 1997, 2000, 2004, 2006, 2009, 2011, 2015)
 #' @return A map with the annual average income mapped on China's map for provinces in the survey. For provinces that are not included in the survey, the color is grey.
 #' @import dplyr
-#' @import leaflet
-#' @import leafletCN
-#' @export
-#'
-#'
+#' @importFrom leafletCN geojsonMap
+#' @export map_income
+#' @examples
+#' # map the province-level average annual income in 2006
+#' map_income_2006 <- map_income(2006)
 
   map_income <- function (year) {
 
--- R/process_datasets.R
+++ R/process_datasets.R
@@ -1,23 +1,21 @@
 #' Annual Income from CHNS survey from 1989 to 2011
 #'
-#' @format ## `indinc_10_clean`
-#' A data frame with 62,029 rows and 32 columns:
+#' @format A data frame with 62,029 rows and 32 columns.
 #' \describe{
-#'   \item{IDind}{individual identification number}
+#'   \item{IDind}{The IDind is a twelve-digit numeric variable that uniquely identified for all participants. Each participant will have the same ID in all datasets and in all survey years. }
 #'   \item{wave}{survey year}
-#'   \item{indwage}{annual wage, bonus, and other income}
+#'   \item{indwage}{nominal annual income}
 #' }
 #' @source <https://www.cpc.unc.edu/projects/china>
 "indinc_10_clean"
 #'
-#'
-#' Number of ever married women and number of children
-#' @format ## `emw_12_clean`
-#' A data frame with 26,616 rows and 60 columns:
+#' Number of Children Data
+#' A dataset that includes number of children for ever-married-women.
+#' @format A data frame with 26,616 rows and 60 columns.
 #' \describe{
-#'   \item{IDind}{individual identification number}
+#'   \item{IDind}{The IDind is a twelve-digit numeric variable that uniquely identified for all participants. Each participant will have the same ID in all datasets and in all survey years. }
 #'   \item{wave}{survey year}
-#'   \item{child_died}{Number of Children who died}
+#'   \item{child_died}{Total number of Children who died}
 #'   \item{child_births}{Total number of Children given births to}
 #'   ...
 #' }
@@ -25,16 +23,16 @@
 "emw_12_clean"
 #'
 #'
-#' Data set for mapping average income on China's map
-#' @format ## `map_dat_clean`
-#' A data frame that could map the average income by selected province and year:
+#' Province-level average income data
+#' Data set with annual average wage for the provinces and years included in the survey.
+#' @format A data frame that could map the average income by selected province and year.
 #' \describe{
-#'   \item{province_en}{province names in english}
+#'   \item{province_en}{province names in English}
 #'   \item{wave}{survey year}
-#'   \item{wage_mean}{intial wage in unit of 1}
-#'   \item{name}{name of provinces in chinese}
-#'   \item{wage_mean_k}{wage in unit of 1000}
-#'   \item{wage_mean_cat}{mean wage in unit of 1000 divided into categories for mapping}
+#'   \item{wage_mean}{Annual average wage in unit of ¥1}
+#'   \item{name}{province names in Chinese}
+#'   \item{wage_mean_k}{Annual average income in unit of ¥1,000}
+#'   \item{wage_mean_cat}{Annual average income in unit of ¥1,000 divided into categories for mapping.}
 #' }
 #' @source <https://www.cpc.unc.edu/projects/china>
 "map_dat_clean"
