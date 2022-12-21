a642fcd84cc7546b51d85744995d2d8785e8c785
Betty Pu <bpu@smith.edu>
2022-12-21 02:55:09 -0500

    edi category

--- R/map_income.R
+++ R/map_income.R
@@ -23,7 +23,7 @@ globalVariables(c("map_dat_clean","indwage","wave","province", "year", "wage_mea
     df <- filter(map_dat_clean, wave == year)
 
     # map the data
-    map <- geojsonMap(df, "china", namevar = ~name, valuevar = ~wage_mean_cat, colorMethod = "factor", legendTitle = "Average Annual Income (unit = ¥1000)", fillOpacity = 0.9)
+    map <- geojsonMap(df, "china", namevar = ~name, valuevar = ~wage_mean_cat, colorMethod = "factor", legendTitle = "Average Annual Income (unit = ¥1000)", palette="RdGy")
     return(map)
   }
 
